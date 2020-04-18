3dd3c6764aa9bbf02a4b0ce1758cf2de4055da79
George Chen <619507631@qq.com>
2020-04-18 15:43:52 +0800

    Add some methods to the Selection to handle with the class attribute. (#15)

--- tests/selection-property.rs
+++ tests/selection-property.rs
@@ -62,3 +62,86 @@ fn test_text() {
 
     assert_eq!(txt.trim(), "Provok.in");
 }
+
+#[test]
+fn test_add_class() {
+    let doc = doc2();
+    let mut sel = doc.select("#main");
+
+    sel.add_class("main main main");
+    let class: &str = &sel.attr("class").unwrap();
+    assert_eq!(class, "main");
+}
+
+#[test]
+fn test_add_class_similar() {
+    let doc = doc2();
+    let mut sel = doc.select("#nf5");
+
+    sel.add_class("odd");
+    println!("{}", sel.html());
+
+    assert!(sel.has_class("odd"));
+    assert!(sel.has_class("odder"));
+}
+
+#[test]
+fn test_add_empty_class() {
+    let doc = doc2();
+    let mut sel = doc.select("#main");
+
+    sel.add_class("");
+    assert!(sel.attr("class").is_none());
+}
+
+#[test]
+fn test_add_classes() {
+    let doc = doc2();
+    let mut sel = doc.select("#main");
+
+    sel.add_class("a b");
+    assert!(sel.has_class("a"));
+    assert!(sel.has_class("b"));
+}
+
+#[test]
+fn test_has_class() {
+    let doc = doc();
+    let sel = doc.select("div");
+    assert!(sel.has_class("span12"));
+}
+
+#[test]
+fn has_class_none() {
+    let doc = doc();
+    let sel = doc.select("toto");
+    assert!(!sel.has_class("toto"));
+}
+
+#[test]
+fn has_class_not_first() {
+    let doc = doc();
+    let sel = doc.select(".alert");
+    assert!(sel.has_class("alert-error"));
+}
+
+#[test]
+fn test_remove_class() {
+    let doc = doc2();
+    let mut sel = doc.select("#nf1");
+    sel.remove_class("one row");
+
+    assert!(sel.has_class("even"));
+    assert!(!sel.has_class("one"));
+    assert!(!sel.has_class("row"));
+}
+
+#[test]
+fn test_remove_class_similar() {
+    let doc = doc2();
+    let mut sel = doc.select("#nf5, #nf6");
+    assert_eq!(sel.length(), 2);
+
+    sel.remove_class("odd");
+    assert!(sel.has_class("odder"));
+}
